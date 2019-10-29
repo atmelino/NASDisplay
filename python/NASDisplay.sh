6ddf1e7ddf390b405417e81db6e3bb454e81847e
nasuser <tmbscience@gmail.com>
2019-10-29 00:09:11 -0500

    service

---
+++ python/NASDisplay.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#sudo chmod 777 /dev/ttyACM0
+
+#python /media/data/github/NASDisplay/python/NASDisplay.py /dev/ttyACM0
+
+python NASDisplay.py 
+
+echo press enter
+
+read input
+
